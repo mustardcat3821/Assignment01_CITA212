e8210db034d7b495c66372265b3daf047e11b269
mustardcat3821 <isaiahmccorkle57@gmail.com>
2026-09-06 14:08:23 -0400

    Added Finishing Touches
    Rigid Body to the Player Game Object and Added walls so the player doesn't run off the map

--- Assignment1_Unity/Assets/Scripts/PlayerMovement.cs
+++ Assignment1_Unity/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.0f;
-    private float horizontalInput;
+    [SerializeField] public float speed = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
     }
 }
